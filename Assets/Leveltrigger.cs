948068969fa5bfd97866d65d3f0f2d01dbd27a80
Schierz, Robert <s65727@beuth-hochschule.de>
2017-10-25 18:27:05 +0200

    Shooting angefangen

---
+++ Assets/Leveltrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leveltrigger : MonoBehaviour {
+
+    public delegate void outOfLevel();
+    public static event outOfLevel onOutoflevel;
+
+    void OnTriggerExit(Collider col)
+    {
+        Destroy(col.gameObject);
+        onOutoflevel();
+    }
+}
---
+++ Assets/ShootBubble.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBubble : MonoBehaviour {
+
+    public GameObject camera;
+    private Transform shootedbubble;
+    public Transform[] bubbleprefabs = new Transform[4];
+    private GameMaster gamemasterattributes;
+
+    private GameObject level;
+
+    public float bubbleforce = 200.0f;
+
+
+    void Start () {
+        this.camera = gameObject;
+        this.level = GameObject.FindGameObjectWithTag("Level");
+        Leveltrigger.onOutoflevel += handleBubbleoutr;
+    }
+	
+	
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shootBubble();
+        }
+
+
+    }
+
+    void handleBubbleoutr()
+    {
+
+        Debug.Log("Bubble hat Level verlassen");
+
+    }
+
+    private void shootBubble()
+    {
+       this.shootedbubble =  Instantiate(randomizePrefabs(), this.camera.transform.position, this.camera.transform.rotation);
+        this.shootedbubble.gameObject.GetComponent<Rigidbody>().AddForce(this.camera.transform.forward * bubbleforce);
+ 
+    }
+
+
+
+    public Transform randomizePrefabs()
+    {
+        int randomprefabindex = UnityEngine.Random.Range(0, 4);
+
+        return this.bubbleprefabs[randomprefabindex];
+    }
+}
