244026d0e8dd590b3289370d2eddf4777b2c441b
Schierz, Robert <s65727@beuth-hochschule.de>
2017-12-29 22:55:33 +0100

    Hauptmenu + Camerafahrt

---
+++ Assets/MoveCameraMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MoveCameraMenu : MonoBehaviour {
+
+    CinemachineVirtualCamera camera;
+    CinemachineTrackedDolly cameraDolly;
+
+    // Use this for initialization
+    void Start () {
+        this.camera = transform.GetComponent<CinemachineVirtualCamera>();
+        this.cameraDolly = this.camera.GetCinemachineComponent<CinemachineTrackedDolly>();
+        this.cameraDolly.m_PathPosition = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.cameraDolly.m_PathPosition != this.cameraDolly.m_Path.MaxPos)
+        {
+            this.cameraDolly.m_PathPosition += Time.deltaTime;
+        }
+         
+		
+	}
+}
