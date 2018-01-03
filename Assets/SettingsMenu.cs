ade53158a721b6c158c4b1234855260f4b2ce91c
Schierz, Robert <s65727@beuth-hochschule.de>
2018-01-03 01:54:53 +0100

    Options fertig

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -3,10 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class SettingsMenu : MonoBehaviour {
 
     private bool playedPreviewSound = false;
+
+    Resolution[] resolutions;
+
+    public TMP_Dropdown resolutionsDropdown;
+
+    void Start()
+    {
+        this.resolutions = Screen.resolutions;
+        this.resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentIndex = 0;
+
+        for (int i = 0; i < this.resolutions.Length; i++)
+        {
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentIndex = i;
+            }
+
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
 
 	public void setMusicVolume(float volume)
     {
@@ -47,6 +79,17 @@ public class SettingsMenu : MonoBehaviour {
     public void setQuality(int quilityIndex)
     {
         QualitySettings.SetQualityLevel(quilityIndex);
+    }
+
+    public void setFullscrean(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
 
